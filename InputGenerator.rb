4bca209c11221b353d40bfc942d9cb763e626258
Huguintoch <huguintoch@users.noreply.github.com>
2020-05-31 14:03:42 -0500

    Adds wheel

--- InputGenerator.rb
+++ InputGenerator.rb
@@ -1,5 +1,4 @@
 if __FILE__ == $0
-   
    n = Integer(ARGV[1])
    if ARGV[0] == "path"
       puts n
@@ -17,5 +16,15 @@ if __FILE__ == $0
       for i in 2..n do
          print "1 #{i} "
       end
+   elsif ARGV[0] == "wheel"
+      puts n
+      for i in 2..n do
+         print "1 #{i} "
+      end
+      for i in 2..n-1 do 
+         print "#{(i)} "
+         print "#{(i+1)} "
+      end
+      print "#{n} 2"
    end
 end
